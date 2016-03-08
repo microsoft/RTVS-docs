1c59bdf56283e650616184e537e971744aefc08f
unknown <mez@mez-x1g3.northamerica.corp.microsoft.com>
2016-03-08 07:46:57 -0500

    change the tryCatch statement to be consistent with other scripts

--- examples/Introduction_to_R_Server/Flight_Delays_Prediction_with_MRS/MRS_flight_delays_with_R_comparison.R
+++ examples/Introduction_to_R_Server/Flight_Delays_Prediction_with_MRS/MRS_flight_delays_with_R_comparison.R
@@ -30,20 +30,19 @@
 
 #---------------------------Step 0: Get Started---------------------------
 # Check the "RevoScaleR" package is loaded in the current RTVS enivronment.
-tryCatch(
-  {
-    library("RevoScaleR")  # Load RevoScaleR package from Microsoft R Server.
-    message("RevoScaleR package is succesfully loaded. Please continue with the further steps.")
-  },
-  error=function(e) {
-    message("RevoScaleR package does not seem to exist...")
-    message("Here's the original error message:")
-    message(paste(e, "\n"))
-    message("If you have Mircrosoft R Server installed, please switch the R engine in R Tools for Visual Studio: R Tools -> Options -> R Engine.")
-    message("If Microsoft R Server is not installed, please download it from here: https://www.microsoft.com/en-us/server-cloud/products/r-server/.")
-    return(NA)
-  }
-)    
+if (require("RevoScaleR"))
+{
+  # Load RevoScaleR package from Microsoft R Server.
+  message("RevoScaleR package is succesfully loaded.\n",
+          "Please continue with the further steps.")
+} else
+{
+  message("RevoScaleR package does not seem to exist...\n", 
+          "If you have Microsoft R Server installed, please switch the R engine\n",
+          "Using R Tools for Visual Studio: R Tools -> Options -> R Engine.\n",
+          "If Microsoft R Server is not installed, please download it from\n",
+          "https://www.microsoft.com/en-us/server-cloud/products/r-server/.")
+}
 
 # Initial some variables.
 github <- "https://raw.githubusercontent.com/Microsoft/RTVS-docs/master/examples/Datasets/"
