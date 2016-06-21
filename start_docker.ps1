e54ee596367cac8c08b829d8e7a81449c5670949
John Lam <jlam@iunknown.com>
2016-06-20 18:33:40 -0700

    Adding a comment to tell the user to ensure that their C drive on host is mapped to the container

--- start_docker.ps1
+++ start_docker.ps1
@@ -1,1 +1,5 @@
+# Note that in Docker for Windows, you must use the Docker Settings app (in the
+# tray) to tell Docker you want to share your C drive, and you will be prompted
+# to enter credentials needed to connect to your C drive.
+
 docker run --env=POLLING=true --name=jekyll --rm --label=jekyll --volume=c:/users/jflam/src/docs:/srv/jekyll -it -p 4000:4000 jekyll/builder:pages
