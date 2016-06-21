f0ca4531e063948a2db75663a1a5a9d9dd71ed1a
John Lam <jlam@iunknown.com>
2016-06-20 22:12:07 -0700

    Updating start_docker powershell command to map the current directory

--- start_docker.ps1
+++ start_docker.ps1
@@ -2,4 +2,4 @@
 # tray) to tell Docker you want to share your C drive, and you will be prompted
 # to enter credentials needed to connect to your C drive.
 
-docker run --env=POLLING=true --name=jekyll --rm --label=jekyll --volume=c:/users/jflam/src/docs:/srv/jekyll -it -p 4000:4000 jekyll/builder:pages
+docker run --env=POLLING=true --name=jekyll --rm --label=jekyll --volume=$(get-location):/srv/jekyll -it -p 4000:4000 jekyll/builder:pages
